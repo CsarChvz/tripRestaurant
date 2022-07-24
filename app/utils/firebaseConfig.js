98aa1a28e37796040f0bb28feeaadb80ecefa8dd
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-07-24 10:51:05 -0500

    Siempre esta pendejada

---
+++ app/utils/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default firebaseConfig = {
+  apiKey: "AIzaSyC6jBSYD5GT1Vi4jFFxcJXAH3z31cfitns",
+  authDomain: "triprest-dacb1.firebaseapp.com",
+  projectId: "triprest-dacb1",
+  storageBucket: "triprest-dacb1.appspot.com",
+  messagingSenderId: "592398204309",
+  appId: "1:592398204309:web:2909987821c5313a570a9c",
+};
